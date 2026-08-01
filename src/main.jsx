dc8462c79687e61f6c4a485a6d4e305143e35641
Malathi <rajamalathi17092026@gmail.com>
2026-08-01 22:30:01 +0530

    Fix i18next initialization import order

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { store } from './redux/store'
+import './i18n/config'  // ← THIS MUST BE HERE, before App renders
 import './index.css'
 import App from './App.jsx'
-import './i18n/config';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
