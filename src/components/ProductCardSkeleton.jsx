9eb2593bafb08460758698250e87a4c49a59c575
Malathi <rajamalathi17092026@gmail.com>
2026-08-13 14:25:33 +0530

    Add loading skeletons

--- src/components/ProductCardSkeleton.jsx
+++ src/components/ProductCardSkeleton.jsx
@@ -1,24 +1,18 @@
 import Skeleton from './Skeleton';
 
-function ProductDetailSkeleton() {
+function ProductCardSkeleton() {
   return (
-    <div className="product-detail-page">
-      <Skeleton width="140px" height="16px" className="skeleton-mb" />
-      <div className="product-detail-container">
-        <div className="product-detail-image">
-          <Skeleton width="100%" height="350px" borderRadius="8px" />
-        </div>
-        <div className="product-detail-info">
-          <Skeleton width="30%" height="12px" className="skeleton-mb" />
-          <Skeleton width="70%" height="28px" className="skeleton-mb" />
-          <Skeleton width="20%" height="24px" className="skeleton-mb" />
-          <Skeleton width="100%" height="60px" className="skeleton-mb" />
-          <Skeleton width="40%" height="14px" className="skeleton-mb" />
-          <Skeleton width="180px" height="44px" borderRadius="4px" />
-        </div>
+    <div className="product-card">
+      <Skeleton width="100%" height="180px" borderRadius="8px 8px 0 0" />
+      <div className="product-info">
+        <Skeleton width="60%" height="12px" className="skeleton-mb" />
+        <Skeleton width="90%" height="16px" className="skeleton-mb" />
+        <Skeleton width="40%" height="18px" className="skeleton-mb" />
+        <Skeleton width="70%" height="12px" className="skeleton-mb" />
+        <Skeleton width="100%" height="36px" borderRadius="4px" />
       </div>
     </div>
   );
 }
 
-export default ProductDetailSkeleton;
+export default ProductCardSkeleton;
